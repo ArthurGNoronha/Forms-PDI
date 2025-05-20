93cde0f69765b121df44853dc5c7844889545bc3
ArthurGNoronha <gattermannoronha@gmail.com>
2025-05-20 17:14:02 -0300

    Feat: Alterado todo o script da página de forms

--- frontend/pages/scripts/scriptForms.js
+++ frontend/pages/scripts/scriptForms.js
@@ -1,233 +1,189 @@
-const respInput = document.getElementById("responsavel");
-const mensagemErroResp = document.getElementById("mensagemErroResp");
-const numeroMedidaInput = document.getElementById("quantidade");
-const mensagemErroQtd = document.getElementById("mensagemErroQtd");
-const mensagemErroReag = document.getElementById("mensagemErroReag");
-var outrosCheckbox = document.getElementById("outros");
-const valorOutros = document.getElementById("outros-texto");
-const mensagemErroOutros = document.getElementById("mensagemErroOutros");
-const MENSAGEM_ERRO_GERAL = "Esse campo é obrigatório!";
-var opcaoSelecionada = "";
-const dropdownIcon = document.getElementById("dropdown-icon");
-const valorPesquisa = document.getElementById("pesquisar");
-const dropdown = document.getElementById("dropdown");
-const options = document.querySelectorAll("#dropdown li");
-let angle = 0
+const reagenteInput = document.getElementById('pesquisar')
+const dropdown = document.getElementById('dropdown');
 
-valorPesquisa.addEventListener('focus', () => {
-  angle += 180;
-  dropdown.style.display = 'block';
-  dropdownIcon.style.transform = `rotate(${angle}deg)`;
+document.addEventListener('DOMContentLoaded', () => {
+  getReagentes();
 });
 
-valorPesquisa.addEventListener('blur', () => {
-  angle += 180;
+reagenteInput.addEventListener('focus', () => {
+  dropdown.style.display = 'block';
+  document.getElementById('dropdownIcon').style.transform = 'rotate(180deg)';
+});
+
+reagenteInput.addEventListener('blur', () => {
   setTimeout(() => {
     dropdown.style.display = 'none';
-    dropdownIcon.style.transform = `rotate(${angle}deg)`;
+    document.getElementById('dropdownIcon').style.transform = 'rotate(0deg)';
   }, 240);
 });
 
-valorPesquisa.addEventListener('input', () => {
-  const escrito = valorPesquisa.value.toLowerCase();
-
-  options.forEach(option => {
-    const optionText = option.innerText.toLowerCase();
-    if (optionText.includes(escrito)) {
-      option.style.display = 'block';
-    } else {
-      option.style.display = 'none';
-    }
-  });
-});
-
-dropdownIcon.addEventListener('click', () => {
-  angle += 180;
+document.getElementById('dropdownIcon').addEventListener('click', () => {
   dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-
-  if (dropdownIcon.style) {
-      dropdownIcon.style.transform = `rotate(${angle}deg)`;
-  }
+  document.getElementById('dropdownIcon').style.transform = dropdown.style.display === 'block' ? 'rotate(180deg)' : 'rotate(0deg)';
+  reagenteInput.focus();
 });
 
-options.forEach(option => {
-  option.addEventListener('click', () => {
-    valorPesquisa.value = '';
-    opcaoSelecionada = option.innerText;
-    valorPesquisa.value = opcaoSelecionada;
-    dropdown.style.display = 'none';
-
-    mensagemErroReag.textContent = "";
-    mensagemErroReag.style.display = 'none';
-  });
-});
-
-document.getElementById("outros").addEventListener("change", () => {
-  var outrosTexto = document.getElementById("outros-texto");
-  outrosTexto.hidden = !this.checked;
-  
-  if (!this.checked) {
-    outrosTexto.value = "";
-    outrosTexto.removeAttribute("required"); 
-  } else {
-    outrosTexto.focus();
-    outrosTexto.setAttribute("required", "required"); 
-  }
-});
-
-var outrasOpcoes = document.querySelectorAll('input[name="medida"]');
-outrasOpcoes.forEach((opcao) => {
-  opcao.addEventListener('change', () => {
-    var outrosTexto = document.getElementById("outros-texto");
-    if (!document.getElementById("outros").checked) {
-      outrosTexto.hidden = true;
-      outrosTexto.value = "";
-      mensagemErroOutros.textContent = "";
-      mensagemErroOutros.style.display = 'none';
+document.querySelectorAll('input[name="medida"]').forEach(input => {
+  input.addEventListener('change', () => {
+    if(document.getElementById('outros').checked) {
+      document.getElementById('outros-texto').hidden = false;
+      document.getElementById('mensagemErroOutros').style.display = 'none';
+    } else {
+      document.getElementById('outros-texto').hidden = true;
+      document.getElementById('outros-texto').value = '';
+      document.getElementById('mensagemErroOutros').style.display = 'none';
     }
   });
 });
 
-numeroMedidaInput.addEventListener("blur", validarNumero);
+function handleInput() {
+  const reagentes = document.querySelectorAll('.reagentes');
+  reagenteInput.addEventListener('input', () => {
+    reagentes.forEach(reagente => {
+      const reagenteValue = reagenteInput.value.trim().toLowerCase();
+      if(reagente.textContent.toLowerCase().includes(reagenteValue)) {
+        reagente.style.display = 'block';
+      } else {
+        reagente.style.display = 'none';
+      }
+    });
+  });
+}
 
-function validarNumero() {
-  let numeroMedidaTexto = numeroMedidaInput.value.trim();
-  
-  numeroMedidaTexto = numeroMedidaTexto.replace(/\./g, ',');
-  
-  numeroMedidaTexto = numeroMedidaTexto.replace(/[^0-9,]/g, '');
-  
-  const numeroMedida = parseFloat(numeroMedidaTexto.replace(/,/g, '.'));
+function handleClick(){
+  const reagentes = document.querySelectorAll('.reagentes');
+  reagentes.forEach(reagente => {
+    reagente.addEventListener('click', () => {
+      let reagenteValue = reagente.textContent.trim().toLowerCase();
+      reagenteInput.value = reagenteValue;
+      dropdown.style.display = 'none';
+      document.getElementById('dropdownIcon').style.transform = 'rotate(0deg)';
+      document.getElementById('mensagemErroReag').style.display = 'none';
+      validateReagente();
+    });
+  });
+}
 
-  mensagemErroQtd.textContent = "";
-  mensagemErroQtd.style.display = 'none';
+function validateQuantidade() {
+  let quantidade = document.getElementById('quantidade').value.trim();
+  quantidade = quantidade.replace(/\./g, ',');
+  quantidade = quantidade.replace(/[^0-9,]/g, '.');
+  quantidade = parseInt(quantidade);
 
-  if (isNaN(numeroMedida) || numeroMedida < 0 || numeroMedida > 9999999) {
-    mensagemErroQtd.textContent = isNaN(numeroMedida)
-      ? "Por favor, insira um número válido."
-      : numeroMedida < 0
-      ? "O número não pode ser negativo!"
-      : "Número é maior do que o permitido!";
-      
-    setTimeout(() => {
-      numeroMedidaInput.value = "";
-      mensagemErroQtd.style.display = 'block';
-    }, 120);
+  document.getElementById('mensagemErroQtd').style.display = 'none';
+
+  if(isNaN(quantidade) || quantidade <= 0 || quantidade > 99999 || quantidade === '') {
+    document.getElementById('mensagemErroQtd').textContent = isNaN(quantidade) ? 'Quantidade inválida' : 'Quantidade deve ser maior que 0';
+    document.getElementById('mensagemErroQtd').style.display = 'block';
   } else {
-    numeroMedidaInput.value = numeroMedidaTexto;
-    mensagemErroQtd.style.display = 'none';
-  }
-}
-  
-dropdown.addEventListener("change",validarReag);
-valorPesquisa.addEventListener("blur", validarReag); 
-  
-function validarReag() {
-  var valorReag = valorPesquisa.value;
-  
-  mensagemErroReag.textContent = "";
-  mensagemErroReag.style.display = 'none';
-  
-  if (valorReag === '') {
-    mensagemErroReag.textContent = MENSAGEM_ERRO_GERAL;
-    mensagemErroReag.style.display = 'block';
-    valorPesquisa.value = "";
+    document.getElementById('mensagemErroQtd').style.display = 'none';
   }
 }
 
-respInput.addEventListener("blur", validarResp);
+function validateReagente() {
+  const reagentes = document.querySelectorAll('.reagentes');
+  const reagente = document.getElementById('pesquisar').value.trim().toLowerCase();
+  document.getElementById('mensagemErroReag').style.display = 'none';
 
-function validarResp(){
-  var nomeResp = respInput.value.trimStart();
+  if (reagente === '') {
+    document.getElementById('mensagemErroReag').textContent = 'Selecione um reagente válido';
+    document.getElementById('mensagemErroReag').style.display = 'block';
+  }
 
-  mensagemErroResp.textContent = "";
-  mensagemErroResp.style.display = 'none';
+  const validOptions = Array.from(reagentes).map(reagente => reagente.textContent.trim().toLowerCase());
+  if(!validOptions.includes(reagente)) {
+    document.getElementById('mensagemErroReag').textContent = 'Selecione um reagente válido';
+    document.getElementById('mensagemErroReag').style.display = 'block';
+  }
+}
 
-  if (nomeResp === '' ){
-    mensagemErroResp.textContent = MENSAGEM_ERRO_GERAL;
-    mensagemErroResp.style.display = 'block';
+function validateResponsavel() {
+  const responsavel = document.getElementById('responsavel').value.trim().toLowerCase();
+  document.getElementById('mensagemErroResp').style.display = 'none';
+
+  if (responsavel === '') {
+    document.getElementById('mensagemErroResp').textContent = 'Digite um nome válido';
+    document.getElementById('mensagemErroResp').style.display = 'block';
   } 
 }
 
-valorOutros.addEventListener("blur", validarOutros);
+function validateOutros() {
+  const outros = document.getElementById('outros-texto').value.trim().toLowerCase();
+  document.getElementById('mensagemErroOutros').style.display = 'none';
 
-function validarOutros() {
-  const outrosInput = valorOutros.value.trimStart();
-  
-  mensagemErroOutros.textContent = "";
-  mensagemErroOutros.style.display = 'none';
-
-  if (outrosCheckbox.checked && outrosInput === "") {
-    setTimeout(() => {
-      mensagemErroOutros.textContent = MENSAGEM_ERRO_GERAL;
-      mensagemErroOutros.style.display = 'block';
-      valorOutros.value = "";
-    }, 62);
+  if (document.getElementById('outros').checked && outros === '') {
+    document.getElementById('mensagemErroOutros').textContent = 'Campo obrigatório';
+    document.getElementById('mensagemErroOutros').style.display = 'block';
   }
 }
 
-outrosCheckbox.addEventListener("change", () => {
-  mensagemErroOutros.textContent = "";
-  mensagemErroOutros.style.display = 'none';
-});
+document.getElementById('quantidade').addEventListener('blur', validateQuantidade);
+document.getElementById('pesquisar').addEventListener('blur', validateReagente);
+document.getElementById('responsavel').addEventListener('blur', validateResponsavel);
+document.getElementById('outros-texto').addEventListener('blur', validateOutros);
 
-document.getElementById("btnEnviar").addEventListener("click", () => {
-  const resp = document.getElementById("responsavel");
-  const valorResp = resp.value.trim();
-  const quantidade = document.getElementById("quantidade");
-  const valorQtd = quantidade.value.trim();
+document.getElementById('btnEnviar').addEventListener('click', () => {
+  const responsavel = document.getElementById('responsavel').value.trim();
+  const quantidade = document.getElementById('quantidade').value.trim();
+  let reagente = document.getElementById('pesquisar').value.trim().toLowerCase();
+  const codigo = reagente.split(' - ')[0].trim();
+  reagente = reagente.split(' - ')[1] ? reagente.split(' - ')[1].trim() : '';
   const medida = document.querySelector('input[name="medida"]:checked');
-  const reagente = document.getElementById("pesquisar");
-  const valorReagente = reagente.value.trim();
-  const outrosCheckbox = document.getElementById("outros");
-  const outrosValor = document.getElementById("outros-texto").value.trimStart();
-  const observacao = document.getElementById("observacao").value;
-  const opcoesValidas = Array.from(options).map(option => option.innerText.toUpperCase().trim());
+  const outros = document.getElementById('outros-texto').value.trim();
+  const observacao = document.getElementById('observacao').value.trim();
 
-  if (valorResp === "" || valorQtd === "" || medida === null || valorReagente === "") {
-    toastAlert('warn', `Por favor, preencha todos os campos obrigatórios antes de enviar!`);
-  } else if (outrosCheckbox.checked && outrosValor === '') {
-    toastAlert('warn', 'A opção "Outros" é obrigatória!');
-  } else if (!opcoesValidas.includes(valorReagente.toUpperCase())) {
-    mensagemErroReag.textContent = 'Por favor, selecione uma opção válida';
-    mensagemErroReag.style.display = 'block';
-    reagente.value = '';
-  } else {
-    var dados = {
-      responsavel: valorResp,
-      quantidade: valorQtd,
-      medida: medida ? medida.value : "",
-      outros: outrosCheckbox.checked ? outrosValor : "",
-      observacao: observacao,
-      reagente: valorReagente
-    };
-
-    fetch('/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dados),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        console.error('Erro ao enviar os dados: ', data.error);
-        toastAlert('error', 'Erro ao enviar os dados');
-
-        if (data.error === 'Reagente Inválido') {
-          toastAlert('error', 'Opção inválida selecionada. Por favor, escolha uma opção válida.');
-          valorReagente = '';
-          mensagemErroReag.textContent = 'Por favor, selecione uma opção válida!';
-          mensagemErroReag.style.display = 'block';
-        }
-      } else if(data.redirect){
-        window.location.href = data.redirect;
-      }
-    })
-    .catch((error) => {
-      toastAlert('error', 'Erro ao enviar os dados');
-      console.error('Erro ao enviar os dados', error);
-    });
+  if(responsavel === '' || quantidade === '' || reagente === '' || !medida || (document.getElementById('outros').checked && outros === '')) {
+    toastAlert('warn', 'Preencha todos os campos corretamente');
+    return;
   }
+
+  fetch('/api/answers/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      responsavel,
+      codigo,
+      reagente,
+      quantidade,
+      medida: medida.value === 'Outros' ? medida.value + ' - ' + outros : medida.value,
+      observacao
+    })
+  })
+  .then((response) => {
+    if(response.ok && response.status === 201) {
+      window.location.href = '/envio';
+    } else {
+      toastAlert('Error', 'Erro ao enviar o formulário');
+    }
+  })
 });
+
+function getReagentes() {
+  fetch('/api/reagentes/forms', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    if(data.error) {
+      console.error('Erro ao buscar os reagentes:', data.error);
+      toastAlert('Error', 'Erro ao buscar os reagentes');
+      return;
+    }
+    if(data) {
+      document.getElementById('dropdown').innerHTML = `
+        ${data.reagentes.map(reagente => `
+          <li class="reagentes">
+            ${reagente.codigo} - ${reagente.reagente}
+          </li>
+        `).join('')}
+      `;
+      handleInput();
+      handleClick();
+    }
+  })
+}
